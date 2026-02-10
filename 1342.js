0c90d84c02e41dcf98d1beae20e3d0dd2540f556
Patel Jivan <patel.jivan.cg@gmail.com>
2026-02-10 18:23:35 +0530

    Question 1342 solved

---
+++ 1342.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let step = 0;
+    while (num > 0) {
+        if(num % 2 == 0) num /= 2;
+        else num--; 
+        step++;
+    }
+    return step;
+};
+
+console.log(numberOfSteps(14))
+console.log(numberOfSteps(8))
