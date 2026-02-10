65da64b6aede78439ac3bf149c097379661b044a
Patel Jivan <patel.jivan.cg@gmail.com>
2026-02-10 23:01:58 +0530

    Question number 2347 solved

---
+++ 2347.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function (nums, diff) {
+    let count = 0;
+    for(let i = 0; i < nums.length - 2; i++) {
+        for(let j = i + 1; j < nums.length - 1; j++) {
+            for(let k = j + 1; k < nums.length; k++) {
+                if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff) count++;
+            }
+        }
+    }
+    return count;
+};
+
+console.log(arithmeticTriplets([0, 1, 4, 6, 7, 10], 3))  // 2
+console.log(arithmeticTriplets([4,5,6,7,8,9], 2))        // 2
