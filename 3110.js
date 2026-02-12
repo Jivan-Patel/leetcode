371d68b3ecfaf4a8834dfb83d43974668df4d260
Patel Jivan <patel.jivan.cg@gmail.com>
2026-02-12 16:59:34 +0530

    Question number 3110 solved

---
+++ 3110.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function (s) {
+    let sum = 0;
+    for (let i = 0; i < s.length - 1; i++)
+        sum += Math.abs(s.charCodeAt(i) - Math.abs(s.charCodeAt(i + 1)));
+    return sum;
+};
+
+console.log(scoreOfString("hello"));
