8e26f83c3cfb23ff600550f02211452055c1612d
Patel Jivan <patel.jivan.cg@gmail.com>
2026-02-18 22:12:43 +0530

    Question number 1952 solved

---
+++ 1952.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isThree = function(n) {
+    let count = 2;
+    for(let i = 2; i <= n/2; i++) {
+        if (n%i===0) {
+            count++;
+            if(count > 3) return false;
+        }
+    }
+    return count === 3;
+};
+
+console.log(isThree(2));
+console.log(isThree(4));
