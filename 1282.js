48a83a0d05116476b745f3e764864a8cbcc450a7
Patel Jivan <patel.jivan.cg@gmail.com>
2026-02-19 18:28:40 +0530

    Question number 1282 solved

--- 1282.js
+++ 1282.js
@@ -8,7 +8,23 @@ var groupThePeople = function (groupSizes) {
         if (obj[groupSizes[i]]) obj[groupSizes[i]].push(i)
         else obj[groupSizes[i]] = [i];
     }
-    return obj;
+    let res = [];
+    for(let key in obj) {
+        if(Number(key) >= obj[key].length) res.push(obj[key]);
+        else {
+            let i = 0;
+            let temp = [[]];
+            for(let size of obj[key]) {
+                if(Number(key) <= temp[i].length) {
+                    temp.push([])
+                    i++;
+                }
+                temp[i].push(size);
+            }
+            res.push(...temp)
+        }
+    }
+    return res;
 };
 
 console.log(groupThePeople([3,3,3,3,3,1,3]));
