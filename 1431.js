6c3689891b03d1481fb154d01d6dc7bcc9be5a65
Patel Jivan <patel.jivan.cg@gmail.com>
2026-02-19 17:43:21 +0530

    Question number 1431 solved

---
+++ 1431.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+    let flag = candies.reduce((max, candy) => (candy > max) ? candy : max, 0) - extraCandies;
+    return candies.map((candy) => candy >= flag);
+};
+
+
+console.log(kidsWithCandies([2, 3, 5, 1, 3], 3));
+console.log(kidsWithCandies([4, 2, 1, 1, 2], 1));
+console.log(kidsWithCandies([12, 1, 12], 10));
