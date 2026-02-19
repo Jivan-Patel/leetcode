b77151cacbe290c80122c59a0e94c2a826df4d75
Patel Jivan <patel.jivan.cg@gmail.com>
2026-02-19 17:37:08 +0530

    Question number 1769 solved

---
+++ 1769.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function (nums) {
+    let pairs = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) pairs++;
+        }
+    }
+    return pairs;
+};
+
+console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3]));
+console.log(numIdenticalPairs([1, 1, 1, 1]));
+console.log(numIdenticalPairs([1, 2, 3]));
