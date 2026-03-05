b4f921ba11b314b0342db5a7c2515f3221f425cf
Patel Jivan <patel.jivan.cg@gmail.com>
2026-03-05 18:56:41 +0530

    Question number 2119 solved

---
+++ cpp/2119.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+bool isSameAfterReversals(int num) {
+    if (num == 0) return true;
+    return num%10 != 0;
+}
+    
+int main() {
+    
+
+    return 0;
+}
