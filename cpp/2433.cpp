1d4d18ab802272a564f899f65c8d0a5e62005866
Patel Jivan <patel.jivan.cg@gmail.com>
2026-03-09 21:16:36 +0530

    Question number 2433 solved

---
+++ cpp/2433.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+vector<int> findArray(vector<int>& pref) {
+    vector<int> result = {pref[0]};
+    for (int i = 1; i < pref.size(); i++) 
+        result.push_back(pref[i - 1] ^ pref[i]);
+    return result;
+}
+    
+int main() {
+    
+
+    return 0;
+}
