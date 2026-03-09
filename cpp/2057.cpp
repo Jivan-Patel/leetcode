f2d1c8d57693e27bc2a6d3fc1befbbeacb72c7c7
Patel Jivan <patel.jivan.cg@gmail.com>
2026-03-09 16:44:01 +0530

    Question number 2057 solved

---
+++ cpp/2057.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int smallestEqual(vector<int>& nums) {
+    for(int i = 0; i < nums.size(); i++) {
+        if(i%10 == nums[i])
+            return i;
+    }
+    return -1;
+}
+
+int main() {
+    
+
+    return 0;
+}
