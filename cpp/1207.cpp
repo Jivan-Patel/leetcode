a5975bbe014f263e6ae89890dddb6398bb7b1cd9
Patel Jivan <patel.jivan.cg@gmail.com>
2026-03-14 21:31:37 +0530

    Question number 1207 solved

--- cpp/1207.cpp
+++ cpp/1207.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
+#include <map>
+#include <vector>
 using namespace std;
 
 class Solution {
+private:
+    map<int,int> frequency(vector<int>& nums) {
+        map<int, int> freq;
+        for(int num: nums) {
+            if(freq[num]) {
+                freq[num]++;
+            }
+            else {
+                freq[num] = 1;
+            }
+        }
+        return freq;
+    }
+
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        
-
-        return false;
+        map<int,int> freq = frequency(arr);
+        map<int,int> freqOfFreq;
+        int freqSize = 0;
+        for (auto& [num, frequency] : freq) {
+            freqSize++;
+            if(freqOfFreq[frequency]) {
+                return false;
+            }
+            else {
+                freqOfFreq[frequency] = 1;
+            }
+        }
+        return true;
     }
 };
 
@@ -19,8 +45,9 @@ int main() {
     vector<int> arr3 = {-3,0,1,-3,1,1,1,-3,10,0};
 
 
-    cout << solution.uniqueOccurrences() << endl;
-    cout << solution.uniqueOccurrences() << endl;
+    cout << solution.uniqueOccurrences(arr1) << endl;
+    cout << solution.uniqueOccurrences(arr2) << endl;
+    cout << solution.uniqueOccurrences(arr3) << endl;
 
     return 0;
 }
