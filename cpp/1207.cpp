8feeced0eac23b57f5e72bda940d4a45985bc2fd
Patel Jivan <patel.jivan.cg@gmail.com>
2026-03-14 18:53:53 +0530

    Updating .gitignore file

---
+++ cpp/1207.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        
+
+        return false;
+    }
+};
+
+int main() {
+
+    Solution solution;
+
+    vector<int> arr1 = {1,2,2,1,1,3};
+    vector<int> arr2 = {1,2};
+    vector<int> arr3 = {-3,0,1,-3,1,1,1,-3,10,0};
+
+
+    cout << solution.uniqueOccurrences() << endl;
+    cout << solution.uniqueOccurrences() << endl;
+
+    return 0;
+}
