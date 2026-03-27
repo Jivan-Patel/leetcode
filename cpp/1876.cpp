b3db9717d9783f584c47aadabb96aae2642a2d18
Patel Jivan <patel.jivan.cg@gmail.com>
2026-03-27 17:10:34 +0530

    Question number 1876 resolve edge case

--- cpp/1876.cpp
+++ cpp/1876.cpp
@@ -5,10 +5,12 @@ using namespace std;
 class Solution {
 public:
     int countGoodSubstrings(string s) {
-        deque <char> dq;
+        if(s.size() < 3) {
+            return 0;
+        }
         int count = 0;
-        for(int i = 0; i <= s.size() - 3; i++) {
-            if (s[i] != s[i+1] && s[i] != s[i+2] && s[i+1] != s[i+2]) {
+        for (int i = 0; i <= s.size() - 3; i++) {
+            if (s[i] != s[i + 1] && s[i] != s[i + 2] && s[i + 1] != s[i + 2]) {
                 count++;
             }
         }
