f818a27ca805943d47993f7cc42fc57d95581c25
Patel Jivan <patel.jivan.cg@gmail.com>
2026-04-10 14:44:03 +0530

    Question number 49 solved with more 2 method

--- cpp/49.cpp
+++ cpp/49.cpp
@@ -3,9 +3,10 @@
 #include <string>
 #include <map>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 
-class Solution {
+class Solution1 {
 private:
     string sortStr(string str) {
         sort(str.begin(), str.end());
@@ -21,6 +22,47 @@ public:
         }
         for (auto& [key, value] : anagram) {
             res.push_back(value);
+        }
+        return res;
+    }
+};
+
+class Solution {    // Best
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string, vector<string>> anagram;
+        for (string str : strs) {
+            string sortedStr = str;
+            sort(sortedStr.begin(), sortedStr.end());            
+            anagram[sortedStr].push_back(str);
+        }
+        for (auto& [key, value] : anagram) {
+            res.push_back(value);
+        }
+        return res;
+    }
+};
+
+
+class Solution2 {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        map<map<char, int>, vector<int>> anagram;
+        for (int i = 0; i < strs.size(); i++) {
+            map<char, int> letterCount;
+            for(char ch: strs[i]) {
+                letterCount[ch]++;
+            }
+            anagram[letterCount].push_back(i);
+        }
+        for (auto& [key, idx] : anagram) {
+            int lastI = res.size();
+            res.push_back({});
+            for(int i : idx) {
+                res[lastI].push_back(strs[i]);
+            }
         }
         return res;
     }
