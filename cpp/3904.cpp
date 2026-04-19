b853ae65f65eb8eb4069086a48a2e222fb39194b
Patel Jivan <patel.jivan.cg@gmail.com>
2026-04-19 21:59:37 +0530

    Question number 3904 solved

---
+++ cpp/3904.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+private:
+    int minIdx(vector<int> nums, int i) {
+        int minim = i;
+        for(int j = i+1; j < nums.size(); j++) {
+            if(nums[minim] > nums[j])
+                minim = j;
+        }
+        return minim;
+    }
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxim = nums[0];
+        int minI = minIdx(nums, 0);
+        
+        for(int i = 0; i < n; i++) {
+            maxim = max(maxim, nums[i]);
+            if(maxim - nums[minI] <= k) return i;
+            if(minI == i) minI = minIdx(nums, i+1);
+        }
+        return -1;
+    }
+};
+
+int main() {
+
+    Solution solution;
+    vector<int> v1 = {5,0,1,4};
+
+    cout << solution.firstStableIndex() << endl;
+
+    return 0;
+}
